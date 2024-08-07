0f5230cb52675b9bb6b3767f32a93b38dd032843
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-07 15:06:40 -0300

    Tutorial 02: Variables

--- Basics/src/Main.cpp
+++ Basics/src/Main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	// Integers
+	int hp = 100;
+	hp += 1;
+
+	// Floats
+	float height = 1.75f;
+
+	float playerDamage = 100.0f;
+	float zombieHealth = 200.0f;
+
+	zombieHealth = zombieHealth - playerDamage;
+ 
+	cout << "The zombie HP is: " << zombieHealth << endl;
+
 	return 0;
 }
