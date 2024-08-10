1e0d18bdcf8d7719bd0cf35f18bfa9ad1e7026a0
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-09 23:47:33 -0300

    Tutorial 12: Arrays for Item Shop

--- Basics/src/Main.cpp
+++ Basics/src/Main.cpp
@@ -1,8 +1,25 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	return 0;
-//}
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	const int numberOfItems = 5;
+
+	string shopItemNames[numberOfItems] = {"Boots", "Sword", "Helmet", "Potion", "Wand"};
+
+	cout << "*****************************" << "\n";
+	cout << "Welcome to the Item Shop!" << "\n";
+	cout << "*****************************" << "\n";
+
+	for (int i = 0; i < numberOfItems; i++) {
+		cout << shopItemNames[i] << endl;
+	}
+
+	cout << "\n";
+	cout << "-----------------------------" << "\n";
+	cout << "What would you like to buy?\n";
+
+	return 0;
+}
--- Basics/src/challenges/GameChallenge01.cpp
+++ Basics/src/challenges/GameChallenge01.cpp
@@ -1,99 +1,99 @@
-#include <iostream>
-#include <string>
-#include <random>
-#include <ctime>
-
-using namespace std;
-
-int getNumberOfSoldiers(string race);
-
-void printResults(int numberOfHumans, int numberOfOrcs);
-
-void simulateBattle(int seed, int& numberOfHumans, int& numberOfOrcs);
-
-int main()
-{
-	static int seed = time(NULL);
-
-	cout << "********************************************" << "\n";
-	cout << "COMBAT SIMULATOR 1D" << "\n";
-	cout << "********************************************" << "\n";
-
-	int numberOfHumans = getNumberOfSoldiers("Humans");
-	int numberOfOrcs = getNumberOfSoldiers("Orcs");
-
-	simulateBattle(seed, numberOfHumans, numberOfOrcs);
-
-	printResults(numberOfHumans, numberOfOrcs);
-
-	return 0;
-}
-
-int getNumberOfSoldiers(string race) {
-	int numberOfSoldiers;
-	cout << "Enter the number of " << race << ":" << "\n";
-	cin >> numberOfSoldiers;
-	return numberOfSoldiers;
-}
-
-void printResults(int numberOfHumans, int numberOfOrcs) {
-	cout << "\n";
-	cout << "--------------------------------------------" << "\n";
-	cout << "The battle is over...\n";
-	if (numberOfHumans > 0) {
-		cout << "The Humans won! " << numberOfHumans << " humans are left in the battlefield...\n\n";
-	}
-	else {
-		cout << "The Orcs won! " << numberOfOrcs << " orcs are left in the battlefield...\n\n";
-	}
-}
-
-void simulateBattle(int seed, int& numberOfHumans, int& numberOfOrcs) {
-	static default_random_engine randomGenerator(seed);
-	uniform_real_distribution<float> dice(0.0f, 1.0f);
-
-	float humanAttack = 0.6f;
-	float humanHealth = 100.0f;
-	float humanDamage = 50.0f;
-	float currentHumanHealth = humanHealth;
-
-	float orcAttack = 0.4f;
-	float orcHealth = 150.0f;
-	float orcDamage = 75.0f;
-	float currentOrcHealth = orcHealth;
-
-	char turn = 'H';
-	float attackRoll;
-
-	while (numberOfHumans > 0 && numberOfOrcs > 0) {
-		//Get our attack result
-		attackRoll = dice(randomGenerator);
-
-		if (turn == 'H') {
-			//Check if attack was successful
-			if (attackRoll <= humanAttack) {
-				currentOrcHealth -= humanDamage;
-
-				if (currentOrcHealth <= 0) {
-					numberOfOrcs--;
-					currentOrcHealth = orcHealth;
-				}
-			}
-
-			turn = 'O';
-		}
-		else {
-			//Check if attack was successful
-			if (attackRoll <= orcAttack) {
-				currentHumanHealth -= orcDamage;
-
-				if (currentHumanHealth <= 0) {
-					numberOfHumans--;
-					currentHumanHealth = humanHealth;
-				}
-			}
-
-			turn = 'H';
-		}
-	}
-}
+//#include <iostream>
+//#include <string>
+//#include <random>
+//#include <ctime>
+//
+//using namespace std;
+//
+//int getNumberOfSoldiers(string race);
+//
+//void printResults(int numberOfHumans, int numberOfOrcs);
+//
+//void simulateBattle(int seed, int& numberOfHumans, int& numberOfOrcs);
+//
+//int main()
+//{
+//	static int seed = time(NULL);
+//
+//	cout << "********************************************" << "\n";
+//	cout << "COMBAT SIMULATOR 1D" << "\n";
+//	cout << "********************************************" << "\n";
+//
+//	int numberOfHumans = getNumberOfSoldiers("Humans");
+//	int numberOfOrcs = getNumberOfSoldiers("Orcs");
+//
+//	simulateBattle(seed, numberOfHumans, numberOfOrcs);
+//
+//	printResults(numberOfHumans, numberOfOrcs);
+//
+//	return 0;
+//}
+//
+//int getNumberOfSoldiers(string race) {
+//	int numberOfSoldiers;
+//	cout << "Enter the number of " << race << ":" << "\n";
+//	cin >> numberOfSoldiers;
+//	return numberOfSoldiers;
+//}
+//
+//void printResults(int numberOfHumans, int numberOfOrcs) {
+//	cout << "\n";
+//	cout << "--------------------------------------------" << "\n";
+//	cout << "The battle is over...\n";
+//	if (numberOfHumans > 0) {
+//		cout << "The Humans won! " << numberOfHumans << " humans are left in the battlefield...\n\n";
+//	}
+//	else {
+//		cout << "The Orcs won! " << numberOfOrcs << " orcs are left in the battlefield...\n\n";
+//	}
+//}
+//
+//void simulateBattle(int seed, int& numberOfHumans, int& numberOfOrcs) {
+//	static default_random_engine randomGenerator(seed);
+//	uniform_real_distribution<float> dice(0.0f, 1.0f);
+//
+//	float humanAttack = 0.6f;
+//	float humanHealth = 100.0f;
+//	float humanDamage = 50.0f;
+//	float currentHumanHealth = humanHealth;
+//
+//	float orcAttack = 0.4f;
+//	float orcHealth = 150.0f;
+//	float orcDamage = 75.0f;
+//	float currentOrcHealth = orcHealth;
+//
+//	char turn = 'H';
+//	float attackRoll;
+//
+//	while (numberOfHumans > 0 && numberOfOrcs > 0) {
+//		//Get our attack result
+//		attackRoll = dice(randomGenerator);
+//
+//		if (turn == 'H') {
+//			//Check if attack was successful
+//			if (attackRoll <= humanAttack) {
+//				currentOrcHealth -= humanDamage;
+//
+//				if (currentOrcHealth <= 0) {
+//					numberOfOrcs--;
+//					currentOrcHealth = orcHealth;
+//				}
+//			}
+//
+//			turn = 'O';
+//		}
+//		else {
+//			//Check if attack was successful
+//			if (attackRoll <= orcAttack) {
+//				currentHumanHealth -= orcDamage;
+//
+//				if (currentHumanHealth <= 0) {
+//					numberOfHumans--;
+//					currentHumanHealth = humanHealth;
+//				}
+//			}
+//
+//			turn = 'H';
+//		}
+//	}
+//}
